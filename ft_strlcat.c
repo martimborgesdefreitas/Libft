35c733e93a21ff4a4f961f2343637c09e9059ad7
Martim Freitas <martimfreitas@hotmail.com>
2024-12-02 23:35:36 +0000

    ft_strlcat.c

---
+++ ft_strlcat.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Untitled-1                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/02 23:23:58 by marvin            #+#    #+#             */
+/*   Updated: 2024/12/02 23:23:58 by marvin           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+size_t	ft_strlcat(char *restrict dst, const char *restrict src, size_t dstsize)
+{
+	size_t	i;
+	size_t	j;
+	size_t	k;
+
+	i = 0;
+	j = 0;
+	while (dst[i] != '\0')
+		i++;
+	while (src[j] != '\0')
+		j++;
+	if (dstsize <= i)
+		return (dstsize + j);
+	if (dstsize > i)
+	{
+		k = 0;
+		while (src[k] != '\0' && i + k + 1 < dstsize)
+		{
+			dst[i + k] = src[k];
+			k++;
+		}
+		dst[i + k] = '\0';
+	}
+	return (i + j);
+}
