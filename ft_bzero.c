d570b30fb7f499397b062409e383cfde0091aa60
Martim Freitas <martimfreitas@hotmail.com>
2024-12-02 20:23:01 +0000

    bzero + norm

--- ft_bzero.c
+++ ft_bzero.c
@@ -14,22 +14,22 @@
 
 void	bzero(void *s, size_t n)
 {
-	size_t              i;
-    unsigned    char    *ptr;
+	size_t			i;
+	unsigned char	*ptr;
 
-    i = 0;
-    ptr = (unsigned char *)s;
-    while (i < n)
-    {
-        ptr[i] = 0;
-        i++;
-    }   
+	i = 0;
+	ptr = (unsigned char *)s;
+	while (i < n)
+	{
+		ptr[i] = 0;
+		i++;
+	}
 }
 
-int main (void)
+/*int main (void)
 {
-    char ptr[] = "123456789";
-    bzero(ptr, 4);
-    printf ("%s\n", ptr + 4);
-    return 0;
-}
+	char ptr[] = "123456789";
+	bzero(ptr, 4);
+	printf ("%s\n", ptr + 4);
+	return 0;
+}*/
