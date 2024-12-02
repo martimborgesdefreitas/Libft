86740d608421c98f449238f56a30bddc51a0ff87
Martim Freitas <martimfreitas@hotmail.com>
2024-12-02 20:09:11 +0000

    bzero

---
+++ ft_bzero.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Untitled-1                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/02 19:39:13 by marvin            #+#    #+#             */
+/*   Updated: 2024/12/02 19:39:13 by marvin           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+void	bzero(void *s, size_t n)
+{
+	size_t              i;
+    unsigned    char    *ptr;
+
+    i = 0;
+    ptr = (unsigned char *)s;
+    while (i < n)
+    {
+        ptr[i] = 0;
+        i++;
+    }   
+}
+
+int main (void)
+{
+    char ptr[] = "123456789";
+    bzero(ptr, 4);
+    printf ("%s\n", ptr + 4);
+    return 0;
+}
