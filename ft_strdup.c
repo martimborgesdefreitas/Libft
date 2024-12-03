26fb42ed2bf6921b0d819725863c6ab219e38080
Martim Freitas <martimfreitas@hotmail.com>
2024-12-03 18:05:18 +0000

    ft_strdup

---
+++ ft_strdup.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Untitled-1                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/03 17:52:30 by marvin            #+#    #+#             */
+/*   Updated: 2024/12/03 17:52:30 by marvin           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+char	*ft_strdup(const char *s)
+{
+	int		i;
+	char	*new_s;
+
+	i = 0;
+	while (s[i] != '\0')
+		i++;
+	new_s = malloc ((i + 1) * sizeof(char));
+	if (new_s == NULL)
+		return (NULL);
+	i = 0;
+	while (s[i] != '\0')
+	{
+		new_s[i] = s[i];
+		i++;
+	}
+	new_s[i] = '\0';
+	return (new_s);
+}
