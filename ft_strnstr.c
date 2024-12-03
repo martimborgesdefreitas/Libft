43512da18a227df77793615cf7b24033169694a6
Martim Freitas <martimfreitas@hotmail.com>
2024-12-03 16:56:29 +0000

    ft_strnstr

---
+++ ft_strnstr.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Untitled-1                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/03 16:40:49 by marvin            #+#    #+#             */
+/*   Updated: 2024/12/03 16:40:49 by marvin           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
+{
+	size_t	i;
+	size_t	j;
+
+	i = 0;
+	if (needle[0] == '\0')
+		return ((char *)haystack);
+	while (haystack[i] != '\0' && i < len)
+	{
+		j = 0;
+		while (needle[j] != '\0' && haystack[i + j] != '\0' && i + j < len)
+		{
+			if (needle[j] == haystack[i + j])
+				j++;
+			else
+				break ;
+		}
+		if (needle[j] == '\0')
+			return ((char *)&haystack[i]);
+		i++;
+	}
+	return (NULL);
+}
