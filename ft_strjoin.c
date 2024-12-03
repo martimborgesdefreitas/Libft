64df5aa0b04e8b73f29bb3da3c885b3a93d0be20
Martim Freitas <martimfreitas@hotmail.com>
2024-12-03 20:14:22 +0000

    ft_strjoin

---
+++ ft_strjoin.c
@@ -0,0 +1,50 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Untitled-1                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/03 18:26:49 by marvin            #+#    #+#             */
+/*   Updated: 2024/12/03 18:26:49 by marvin           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+size_t	ft_strlen(const char *s)
+{
+	int	i;
+
+	i = 0;
+	while (s[i] != '\0')
+		i++;
+	return (i);
+}
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	size_t	tlen;
+	size_t	i;
+	size_t	j;
+	char	*cat;
+
+	tlen = ft_strlen(s1) + ft_strlen(s2);
+	cat = malloc((tlen + 1) * sizeof(char));
+	if (cat == NULL)
+		return (NULL);
+	i = 0;
+	while (s1[i] != '\0')
+	{
+		cat[i] = s1[i];
+		i++;
+	}
+	j = 0;
+	while (s2[j] != '\0')
+	{
+		cat[i + j] = s2[j];
+		j++;
+	}
+	cat[i + j] = '\0';
+	return (cat);
+}
