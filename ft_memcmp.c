e76d93cd94e183dced80d74755d3c6b4770497a0
Martim Freitas <martimfreitas@hotmail.com>
2024-12-03 16:40:27 +0000

    ft_memcmp

---
+++ ft_memcmp.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Untitled-1                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/03 16:23:59 by marvin            #+#    #+#             */
+/*   Updated: 2024/12/03 16:23:59 by marvin           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_memcmp(const void *s1, const void *s2, size_t n)
+{
+	unsigned char	*str1;
+	unsigned char	*str2;
+	size_t			i;
+
+	str1 = (unsigned char *)s1;
+	str2 = (unsigned char *)s2;
+	i = 0;
+	while (i < n)
+	{
+		if (str1[i] != str2[i])
+			return (str1[i] - str2[i]);
+		i++;
+	}
+	return (0);
+}
