d6ff7d5ded6eba2ab9aabc10b180ade7696a6d56
Martim Freitas <martimfreitas@hotmail.com>
2024-12-03 16:24:12 +0000

    ft_memchr

---
+++ ft_memchr.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Untitled-1                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/03 16:09:29 by marvin            #+#    #+#             */
+/*   Updated: 2024/12/03 16:09:29 by marvin           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	*memchr(const void *s, int c, size_t n)
+{
+	unsigned char	cc;
+	unsigned char	*ptr;
+	size_t			i;
+
+	ptr = (unsigned char *)s;
+	cc = (unsigned char)c;
+	i = 0;
+	while (i < n)
+	{
+		if (ptr[i] == cc)
+			return ((void *)&ptr[i]);
+		i++;
+	}
+	return (NULL);
+}
