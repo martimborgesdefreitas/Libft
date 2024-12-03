c741265cd7672242f8c70e6a0b7a91a00d319fda
Martim Freitas <martimfreitas@hotmail.com>
2024-12-03 21:14:57 +0000

    ft_strtrim

---
+++ ft_strtrim.c
@@ -0,0 +1,64 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Untitled-1                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/03 20:16:24 by marvin            #+#    #+#             */
+/*   Updated: 2024/12/03 20:16:24 by marvin           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+int	is_set(char a, const char *set)
+{
+	int	i;
+
+	i = 0;
+	while (set[i] != '\0')
+	{
+		if (set[i] == a)
+			return (1);
+		i++;
+	}
+	return (0);
+}
+
+size_t	ft_strlen(const char *s)
+{
+	int	i;
+
+	i = 0;
+	while (s[i] != '\0')
+		i++;
+	return (i);
+}
+
+char	*ft_strtrim(char const *s1, char const *set)
+{
+	int		end;
+	int		len;
+	int		start;
+	char	*str;
+
+	start = 0;
+	if (s1 == NULL)
+		return (NULL);
+	while (s1[start] != '\0' && is_set(s1[start], set) == 1)
+		start++;
+	end = ft_strlen(s1) - 1;
+	while (is_set(s1[end], set) == 1 && end >= start)
+		end--;
+	len = end - start + 1;
+	if (len <= 0)
+		len = 0;
+	str = malloc((len + 1) * sizeof(char));
+	if (str == NULL)
+		return (NULL);
+	str[len] = '\0';
+	while (--len >= 0)
+		str[len] = s1[start + len];
+	return (str);
+}
