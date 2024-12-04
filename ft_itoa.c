de778c8bc9acedd6e9ec715be66464543435c296
Martim Freitas <martimfreitas@hotmail.com>
2024-12-04 00:02:20 +0000

    ft_itoa INT_MIN?

---
+++ ft_itoa.c
@@ -0,0 +1,63 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Untitled-1                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/03 22:40:10 by marvin            #+#    #+#             */
+/*   Updated: 2024/12/03 22:40:10 by marvin           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+int	num_len(int n)
+{
+	int	len;
+
+	len = 0;
+	if (n == 0)
+		return (1);
+	if (n == -2147483648)
+	{
+		len = 11;
+		return (len);
+	}
+	if (n < 0)
+	{
+		len++;
+		n = -n;
+	}
+	while (n != 0)
+	{
+		n = n / 10;
+		len++;
+	}
+	return (len);
+}
+
+char	*ft_itoa(int n)
+{
+	int		len;
+	char	*str;
+
+	len = num_len(n);
+	str = malloc((len + 1) * sizeof(char));
+	if (str == NULL)
+		return (NULL);
+	str[len] = '\0';
+	if (n == 0)
+		str[0] = '0';
+	if (n < 0)
+		str[0] = '-';
+	while (n != 0)
+	{
+		if (n < 0)
+			str[--len] = (n % 10) * (-1) + 48;
+		else
+			str[--len] = (n % 10) + 48;
+		n = n / 10;
+	}
+	return (str);
+}
